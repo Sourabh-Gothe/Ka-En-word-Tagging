2554188cda08902c69929dbc3fd573bb0838a883
Sourabh-Gothe <sourabhgothe@gmail.com>
2017-02-25 04:53:14 +0530

    Update base.py

--- base.py
+++ base.py
@@ -1,30 +1,67 @@
+from gensim.models import word2vec
+from collections import OrderedDict
+from nltk.classify import SklearnClassifier
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.svm import SVC
+import numpy as np
+from sklearn.metrics import accuracy_score
+
 import nltk
-#from tkinter import *
-from nltk.corpus import stopwords
+import token
 
-
-
-def read():
+def read(filename):
     fp = open(filename,"r")
-    f = fp.read()
-    t2 = nltk.word_tokenize(f)
-    prt = nltk.PorterStemmer()
-    #t1 = [prt.stem(t) for t in t1]
-    t3 = nltk.pos_tag(t2)
-    stop = stopwords.words('english')
-    stop.append("By")
-    list1 = []
-    for (word, tag) in t3:
-        if tag[:3] == "NNP" or tag[:3] =="NN" and word not in stop:
-            list1.append(word)
-    op = open("output.txt","w")
-    list2 = []
-    for word in list1:
-        if word not in list2 and word not in stop:
-            list2.append(word)
-            op.write(word+"\n")
-            print word
-    op.close()
+    f = fp.readlines()  
+    vocab = [s.encode('utf-8').split() for s in f]
+    #print vocab
+    voc_vec = word2vec.Word2Vec(vocab,min_count=1,size=4)
+    #print voc_vec.syn0.shape
+    #print type(voc_vec['yav'])
+    #Openning data file
     fp.close()
-filename = "a.txt"
-read()
+    fp = open("test_data.txt","r")
+    f = fp.read()
+    tokens = nltk.word_tokenize(f)
+    D = OrderedDict()
+    sentences = []
+    #print len(tokens)
+    for word in tokens[0:200]:
+        D[word.split("|")[0]] = word.split("|")[1]
+        sentences.append(word.split("|")[0])
+    #print D
+    
+    train_data = []
+    
+    for key in D:
+        l = voc_vec[key]
+        x = {}
+        x['a'] =  l[0]
+        x['b'] =  l[1]
+        x['c'] =  l[2]
+        x['d'] =  l[3]
+        train_data.append((x,D[key]))
+    classif = SklearnClassifier(BernoulliNB()).train(train_data)
+    #print train_data
+    
+    
+    test_data = []
+    D2 = OrderedDict()
+    for word in tokens[200:300]:
+        D2[word.split("|")[0]] = word.split("|")[1]
+    expected_list = []
+    for key in D2:
+        l = voc_vec[key]
+        x = {}
+        x['a'] =  l[0]
+        x['b'] =  l[1]
+        x['c'] =  l[2]
+        x['d'] =  l[3]
+        test_data.append(x)
+        expected_list.append(D2[key])
+    predicted =  classif.classify_many(test_data)
+    print len(predicted)
+    print len(expected_list)
+    print accuracy_score(expected_list, predicted ,normalize=False)
+
+
+test =  read("plain_data.txt")
