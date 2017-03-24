70204dca6b2ffe111be4dd99aadbc9b81d654866
Sourabh-Gothe <sourabhgothe@gmail.com>
2017-03-24 16:01:35 +0530

    Update base.py

--- base.py
+++ base.py
@@ -1,67 +1,72 @@
+from __future__ import division, print_function, absolute_import
+import tflearn
+from tflearn.data_utils import to_categorical, pad_sequences
 from gensim.models import word2vec
 from collections import OrderedDict
-from nltk.classify import SklearnClassifier
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.svm import SVC
 import numpy as np
-from sklearn.metrics import accuracy_score
-
-import nltk
 import token
 
-def read(filename):
+import tflearn
+from tflearn.data_utils import to_categorical, pad_sequences
+import nltk
+
+def GetTrainData(filename):
     fp = open(filename,"r")
-    f = fp.readlines()  
-    vocab = [s.encode('utf-8').split() for s in f]
-    #print vocab
-    voc_vec = word2vec.Word2Vec(vocab,min_count=1,size=4)
-    #print voc_vec.syn0.shape
-    #print type(voc_vec['yav'])
-    #Openning data file
-    fp.close()
-    fp = open("test_data.txt","r")
     f = fp.read()
     tokens = nltk.word_tokenize(f)
-    D = OrderedDict()
-    sentences = []
-    #print len(tokens)
     for word in tokens[0:200]:
-        D[word.split("|")[0]] = word.split("|")[1]
-        sentences.append(word.split("|")[0])
-    #print D
-    
-    train_data = []
-    
-    for key in D:
-        l = voc_vec[key]
-        x = {}
-        x['a'] =  l[0]
-        x['b'] =  l[1]
-        x['c'] =  l[2]
-        x['d'] =  l[3]
-        train_data.append((x,D[key]))
-    classif = SklearnClassifier(BernoulliNB()).train(train_data)
-    #print train_data
-    
-    
-    test_data = []
-    D2 = OrderedDict()
-    for word in tokens[200:300]:
-        D2[word.split("|")[0]] = word.split("|")[1]
-    expected_list = []
-    for key in D2:
-        l = voc_vec[key]
-        x = {}
-        x['a'] =  l[0]
-        x['b'] =  l[1]
-        x['c'] =  l[2]
-        x['d'] =  l[3]
-        test_data.append(x)
-        expected_list.append(D2[key])
-    predicted =  classif.classify_many(test_data)
-    print len(predicted)
-    print len(expected_list)
-    print accuracy_score(expected_list, predicted ,normalize=False)
+        training_words.append(word.split("|")[0])
+        pre_defined_tags.append(word.split("|")[1])
 
 
-test =  read("plain_data.txt")
+
+
+def GetVectors(filename):
+    fp = open(filename,"r")
+    file = fp.readlines()
+    vocab = [line.split() for line in file]
+    vectors = word2vec.Word2Vec(vocab,min_count=1,size=3)
+    #print voc_vec.syn0.shape
+    for word in training_words:
+        trainData.append(vectors[word])
+
+
+
+#Training data
+training_words   = []
+#Target data
+pre_defined_tags = []
+GetTrainData("train_data.txt")
+
+#Converting Words into Vectors
+trainData  = []
+TargetData = pre_defined_tags
+GetVectors("plain_data.txt")
+
+testData = trainData[150:200]
+testTargetData = TargetData[150:200]
+
+# Data preprocessing
+# Sequence padding
+trainX = pad_sequences(trainData[:150], maxlen=100, value=0.)
+testX = pad_sequences(testData, maxlen=100, value=0.)
+# Converting labels to binary vectors
+trainY = to_categorical(TargetData, nb_classes=2)
+testY = to_categorical(testTargetData, nb_classes=2)
+
+
+# Network building
+net = tflearn.input_data([None, 100])
+net = tflearn.embedding(net, input_dim=10000, output_dim=128)
+net = tflearn.lstm(net, 128, dropout=0.8)
+net = tflearn.fully_connected(net, 2, activation='softmax')
+net = tflearn.regression(net, optimizer='adam', learning_rate=0.001,
+                         loss='categorical_crossentropy')
+
+model = tflearn.DNN(net, tensorboard_verbose=3)
+model.fit(trainX, trainY, validation_set=(testX, testY), show_metric=True,
+          batch_size=5)
+print(testX)
+print(model.predict(testX))
+
+
